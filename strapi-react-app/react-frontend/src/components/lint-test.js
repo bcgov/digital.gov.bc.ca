081381a7e4db60e6622e0dd7b68536f852b2aa8b
Ryley Woodland <ryleywoodland@gmail.com>
2020-05-27 15:36:32 -0700

    test 7

--- strapi-react-app/react-frontend/src/components/lint-test.js
+++ strapi-react-app/react-frontend/src/components/lint-test.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 
 function LintTest() {
-    const constant = 50;
+    const constant = 5;
     return(
         <div className="LinterTest"></div> 
     )
