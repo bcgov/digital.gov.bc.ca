5b06baf5f802379c92fb763203d67d5aee0406ad
jsharman <jon.w.sharman@gmail.com>
2020-06-04 12:06:54 -0700

    React app accessing env properly

--- react-frontend/src/containers/App/index.js
+++ react-frontend/src/containers/App/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import Nav from '../../components/Nav'
 
 function App() {
+  console.log('The env is:')
+  console.log(process.env)
+
   return (
     <div className="App" >
       <p>THIS IS A TEST APP</p>
--- react-frontend/src/utils/apolloClient.js
+++ react-frontend/src/utils/apolloClient.js
@@ -1,7 +1,7 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-  uri: `${process.env.STRAPI_BACKEND_URL}/graphql` || "http://localhost:1337/graphql",
+  uri: process.env.REACT_APP_STRAPI_BACKEND_URL || "http://localhost:1337/graphql",
   // uri: "http://localhost:1337/graphql",
 });
 
