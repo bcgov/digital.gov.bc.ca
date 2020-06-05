a94e42eac3c417e3350a10a05e0cc0f12eecbc15
jsharman <jon.w.sharman@gmail.com>
2020-06-05 16:49:05 -0700

    Hot reloading now works'

--- react-frontend/src/containers/App/index.js
+++ react-frontend/src/containers/App/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import Nav from '../../components/Nav';
 
 function App() {
-  console.log('The env is:')
-  console.log(process.env)
-
   return (
     <div className="App">
       <p>THIS IS A TEST APP</p>
