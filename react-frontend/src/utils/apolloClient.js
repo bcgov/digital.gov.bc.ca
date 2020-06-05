57fcfe3b3f82fa5ab2655c9fde848a2c1cdf203d
jsharman <jon.w.sharman@gmail.com>
2020-06-05 16:51:45 -0700

    Fix the docker file

--- react-frontend/src/utils/apolloClient.js
+++ react-frontend/src/utils/apolloClient.js
@@ -1,7 +1,8 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_STRAPI_BACKEND_URL || "http://localhost:1337/graphql",
+  uri:
+    process.env.REACT_APP_STRAPI_BACKEND_URL || 'http://localhost:1337/graphql',
 });
 
 export default client;
