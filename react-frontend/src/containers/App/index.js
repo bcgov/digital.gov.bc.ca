9c66e19ebde8dfd1ccbe1e73bcede1a3c347a2fb
Ryley Woodland <ryleywoodland@gmail.com>
2020-06-15 10:09:54 -0700

    commented out code for demo

--- react-frontend/src/containers/App/index.js
+++ react-frontend/src/containers/App/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Nav from '../../components/Nav';
+//import Nav from '../../components/Nav';
 import Home from '../../components/Home/home.js';
 
 function App() {
   return (
     <div className="App">
-      <p>THIS IS A TEST APP</p>
-      <Nav />
+      {/* <p>THIS IS A TEST APP</p>
+      <Nav /> */}
       <Home />
     </div>
   );
