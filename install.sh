d0ea203878c72b6c9d1bddbc1b47a05b89c6b30c
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2020-06-19 11:46:14 -0700

    begin documenting the cicd workflow

---
+++ install.sh
@@ -0,0 +1,15 @@
+curl "https://mirror.openshift.com/pub/openshift-v3/clients/3.11.232/linux/oc.tar.gz" > oc.tar.gz
+tar xvzf oc.tar.gz 
+rm oc.tar.gz
+mv oc /usr/local/bin
+
+# Validate the installation
+echo "Validate the installation"
+if ! command -v oc; then
+    echo "oc was not installed"
+    exit 1
+fi
+
+# Document the installed version
+echo "Document the installed version"
+DocumentInstalledItem "oc CLI ($(oc --version 2>&1))"
