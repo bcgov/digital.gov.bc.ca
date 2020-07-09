3f80989267e8e930489351a157f07b7a9001c93f
jsharman <jon.w.sharman@gmail.com>
2020-07-09 08:59:58 -0700

    Implemented patrick changes

--- react-frontend/src/components/Guides/CollaborationTools/collaborationTool.js
+++ react-frontend/src/components/Guides/CollaborationTools/collaborationTool.js
@@ -1,28 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function CollaborationTool(props) {
-  return (
-    <div className="collaborationTool">
-      <a href={props.url}>
-        <strong>{props.name}</strong>
-      </a>{' '}
-      *
-      <ul>
-        <li>
-          <strong>Cost</strong>: {props.cost}
-        </li>
-        <li>
-          <strong>Platform</strong>: {props.platform}
-        </li>
-        <li>
-          <strong>Capacity</strong>: {props.capacity}
-        </li>
-        <li>
-          <strong>Features</strong>: {props.features}
-        </li>
-      </ul>
-    </div>
-  );
-}
+const CollaborationTool = ({
+  url,
+  name,
+  cost,
+  platform,
+  capacity,
+  features,
+}) => (
+  <div className="collaborationTool">
+    <a href={url}>
+      <strong>{name}</strong>
+    </a>{' '}
+    *
+    <ul>
+      <li>
+        <strong>Cost</strong>: {cost}
+      </li>
+      <li>
+        <strong>Platform</strong>: {platform}
+      </li>
+      <li>
+        <strong>Capacity</strong>: {capacity}
+      </li>
+      <li>
+        <strong>Features</strong>: {features}
+      </li>
+    </ul>
+  </div>
+);
 
 export default CollaborationTool;
