752b6da085f57817b2c09de990e9bff1edb0c852
jsharman <jon.w.sharman@gmail.com>
2020-07-22 22:03:38 -0700

    Remove width specification

--- react-frontend/src/components/Guides/CollaborationTools/collaborationTools.js
+++ react-frontend/src/components/Guides/CollaborationTools/collaborationTools.js
@@ -21,25 +21,21 @@ const Title = styled.h1`
   font-size: 3rem;
   font-weight: 700;
   margin-top: 3rem;
-  width: 100%;
 `;
 const Title2 = styled.h2`
   font-size: 3rem;
   font-weight: 700;
   margin-top: 3rem;
-  width: 100%;
 `;
 const Title3 = styled.h3`
   font-size: 2.5rem;
   font-weight: 700;
   margin-top: 3rem;
-  width: 100%;
 `;
 const Title4 = styled.h4`
   font-size: 2.25rem;
   font-weight: 700;
   margin-top: 2.75rem;
-  width: 100%;
 `;
 
 function CollaborationTools() {
