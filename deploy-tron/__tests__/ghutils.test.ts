8bfc1c902696f744e20827f8ff7b776e0ab54be8
Patrick Simonian <patricksimonian@gmail.com>
2020-08-13 13:48:15 -0700

    improve test

--- deploy-tron/__tests__/ghutils.test.ts
+++ deploy-tron/__tests__/ghutils.test.ts
@@ -3,7 +3,7 @@ import {Context} from 'probot';
 import {pullRequestComment} from '../__fixtures__/pull_request_comment';
 import {issueComment} from '../__fixtures__/issue_comment';
 import {github} from './helpers';
-import {adminUser, readUser} from '../__fixtures__/collaborator';
+import {adminUser, readUser, writeUser, maintainUser} from '../__fixtures__/collaborator';
 import config from '../__fixtures__/config.json';
 
 describe('Gh Utilities', () => {
@@ -20,6 +20,10 @@ describe('Gh Utilities', () => {
   test('return true if user permission is allowable', async () => {
     const context = new Context(pullRequestComment, github as any, {} as any);
     github.repos.getCollaboratorPermissionLevel.mockReturnValueOnce(Promise.resolve({data: adminUser}));
+    github.repos.getCollaboratorPermissionLevel.mockReturnValueOnce(Promise.resolve({data: writeUser}));
+    github.repos.getCollaboratorPermissionLevel.mockReturnValueOnce(Promise.resolve({data: maintainUser}));
+    expect(await isCommenterAllowedToAction(context, config.validGithubRoles)).toBe(true);
+    expect(await isCommenterAllowedToAction(context, config.validGithubRoles)).toBe(true);
     expect(await isCommenterAllowedToAction(context, config.validGithubRoles)).toBe(true);
   });
 
