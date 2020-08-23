40d034fb9b8f520a76147bf816ba133fc9eb6818
Patrick Simonian <patricksimonian@gmail.com>
2020-08-23 16:38:51 -0700

    Add new help commands and fix tests

--- deploy-tron/src/index.ts
+++ deploy-tron/src/index.ts
@@ -1,9 +1,11 @@
 import { Application } from 'probot'; // eslint-disable-line no-unused-vars
-import { handlePrComment } from './handlers';
+import { handlePrComment, handlePrOpened } from './handlers';
 
 export = (app: Application): void => {
 
   app.on('issue_comment.created', handlePrComment);
+
+  app.on('pull_request.opened', handlePrOpened);
   // For more information on building apps:
   // https://probot.github.io/docs/
 
