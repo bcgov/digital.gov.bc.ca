5fbbd24e9fa84560435a060434cdd3d6cce19db6
jsharman <jon.w.sharman@gmail.com>
2020-09-16 15:15:29 -0700

    Reconfigure the async apollo component to leverage useEffect and use state

--- react-frontend/src/providers/ApolloProviderAsync.js
+++ react-frontend/src/providers/ApolloProviderAsync.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
 
@@ -8,20 +8,26 @@ import { AppConfigContext } from './AppConfig';
 // by pulling the backend url from a config file. This allows
 // the deployment config file instead of being baked into
 // the build config.
+
 function ApolloProviderAsync(props) {
   const config = useContext(AppConfigContext);
-
   const strapiURL = config['state']['strapiApiUrl'];
 
-  let client = new ApolloClient({
-    uri: 'http://localhost:3000/graphql',
-  });
+  // using client as state prevents it from being initialized needlessly
+  const [client, setClient] = useState(
+    new ApolloClient({
+      uri: 'http://localhost:3000/graphql',
+    })
+  );
 
-  if (strapiURL) {
-    client = new ApolloClient({
-      uri: strapiURL,
-    });
-  }
+  // The useEffect is only called when config changes
+  useEffect(() => {
+    if (strapiURL) {
+      setClient(new ApolloClient({ uri: strapiURL }));
+    }
+    console.log('This was called!');
+  }, [config, strapiURL]);
+
   return <ApolloProvider client={client}>{props.children}</ApolloProvider>;
 }
 
