91e8f3832a6bfeb54453d3f2b6208d9b1e592ae1
jsharman <jon.w.sharman@gmail.com>
2020-10-08 15:03:22 -0700

    Remove uneeded col import

--- react-frontend/src/components/StyleComponents/htmlTags.js
+++ react-frontend/src/components/StyleComponents/htmlTags.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { Col } from 'react-flexbox-grid';
 
 // This file contains styling for Link, a, ul, li, col, row html tags
 
