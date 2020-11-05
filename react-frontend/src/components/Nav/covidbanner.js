c97836bf984d88b1a16c07a05228812a2ddaa0a8
jsharman <jon.w.sharman@gmail.com>
2020-11-05 08:44:09 -0800

    made the code more legible

--- react-frontend/src/components/Nav/covidbanner.js
+++ react-frontend/src/components/Nav/covidbanner.js
@@ -12,6 +12,22 @@ function CovidBanner(props) {
   const [showBanner, setShowBanner] = useState(true);
   const [breadCrumb, setBreadCrumb] = useState(true);
 
+  const covidHealthLink =
+    'http://covid-19.bccdc.ca/?utm_campaign=20200323_GCPE_AM_COVID_7_NOTIFICATION_WORDPRESS_BCGOV_EN_BC__NOTIFICATION';
+  const covidBCresponseLink =
+    'https://www2.gov.bc.ca/gov/content/safety/emergency-preparedness-response-recovery/covid-19-provincial-support?utm_campaign=20200323_GCPE_AM_COVID_8_NOTIFICATION_WORDPRESS_BCGOV_EN_BC__NOTIFICATION';
+
+  const closeBanner = (event) => {
+    event.preventDefault();
+    setBreadCrumb(true);
+    setShowBanner(false);
+    const nav = document.getElementById('navbar');
+    nav.style.top = '67px';
+    const header = document.getElementById('navheader');
+    header.style.top = 0;
+    props.test(breadCrumb);
+  };
+
   if (showBanner) {
     return (
       <CovidAlert
@@ -23,27 +39,16 @@ function CovidBanner(props) {
               </CovidCol>
               <CovidCol xs={10}>
                 B.C. has declared a state of emergency. Learn about{' '}
-                <CovidLinkStyle href="http://covid-19.bccdc.ca/?utm_campaign=20200323_GCPE_AM_COVID_7_NOTIFICATION_WORDPRESS_BCGOV_EN_BC__NOTIFICATION">
+                <CovidLinkStyle href={covidHealthLink}>
                   COVID-19 health issues.
                 </CovidLinkStyle>{' '}
                 |{' '}
-                <CovidLinkStyle href="https://www2.gov.bc.ca/gov/content/safety/emergency-preparedness-response-recovery/covid-19-provincial-support?utm_campaign=20200323_GCPE_AM_COVID_8_NOTIFICATION_WORDPRESS_BCGOV_EN_BC__NOTIFICATION">
-                  B.C.'s response to COVID-19.{' '}
+                <CovidLinkStyle href={covidBCresponseLink}>
+                  B.C.'s response to COVID-19.
                 </CovidLinkStyle>
               </CovidCol>
               <CovidCol xs={1}>
-                <CovidLinkStyleButton
-                  onClick={(event) => {
-                    event.preventDefault();
-                    setBreadCrumb(true);
-                    setShowBanner(false);
-                    const nav = document.getElementById('navbar');
-                    nav.style.top = '67px';
-                    const header = document.getElementById('navheader');
-                    header.style.top = 0;
-                    props.test(breadCrumb);
-                  }}
-                >
+                <CovidLinkStyleButton onClick={closeBanner}>
                   X
                 </CovidLinkStyleButton>
               </CovidCol>
