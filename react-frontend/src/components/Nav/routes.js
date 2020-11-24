c3d719a2a8d23c5b6bb5d5a6c476da95c37d2b47
jsharman <jon.w.sharman@gmail.com>
2020-11-24 12:51:20 -0800

    WIP

--- react-frontend/src/components/Nav/routes.js
+++ react-frontend/src/components/Nav/routes.js
@@ -10,6 +10,7 @@ import DigitalPrinciples from '../../components/DigitalPrinciples/digitalPrincip
 // import GuidesPage from '../../components/Guides/guides';
 // import GuidePage from '../../components/Guides/guide';
 import BreadCrumbs from './breadcrumbs';
+import NotFound from '../NotFoundPage/notFoundPage'
 
 const DisplayNames = {
   'case-studies': 'Case Studies',
@@ -26,34 +27,23 @@ const Routes = () => {
     <div>
       <BreadCrumbs />
       <Switch>
-        <Route path="/products-services">
-          <Products />
-        </Route>
-        <Route path="/digital-framework">
-          <DigitalFramework />
-        </Route>
-        <Route path="/resources/digital-principles">
-          <DigitalPrinciples />
-        </Route>
-        <Route path="/resources">
-          <Resources />
-        </Route>
-        <Route path="/case-studies">
-          <CaseStudies />
-        </Route>
-        <Route path="/guides/communication-platforms">
-          <CollaborationTools />
-        </Route>
+        <Route exact path="/products-services" component={Products} />
+        <Route exact path="/digital-framework" component={DigitalFramework} />
+        <Route exact path="/resources/digital-principles" component={DigitalPrinciples} />
+        <Route exact path="/resources" component={Resources} />
+        <Route path="/case-studies" component={CaseStudies} />
+        <Route exact path="/guides/communication-platforms" component={CollaborationTools} />
         {/* This is a test guides page, KEEP comented out until strapi is implemented */}
-        {/* <Route path="/guides/:uid" exact>
-        <GuidePage />
-      </Route>
-      <Route path="/guides" exact>
-        <GuidesPage />
-      </Route> */}
-        <Route path="/">
-          <Home />
+        {/* 
+        <Route path="/guides/:uid" exact>
+          <GuidePage />
         </Route>
+        <Route exact path="/guides" component={GuidesPage} /> 
+        */}
+        <Route exact path="/" component={Home} />
+        {/* TODO: REDIRECT STANDARDS AND GUIDES LINK TO RESOURCE PAGE */}
+        <Route path="/standards-and-guides" component={NotFound} />
+        <Route component={NotFound} />
       </Switch>
     </div>
   );
