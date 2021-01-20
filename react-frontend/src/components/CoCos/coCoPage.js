e74884e89cefc3009a03f34747987591778b1599
jsharman <jon.w.sharman@gmail.com>
2021-01-20 13:34:52 -0800

    WIP

---
+++ react-frontend/src/components/CoCos/coCoPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CoCoCards from './coCoCards.js';
+import DocumentTitle from 'react-document-title';
+import BannerSideImage from '../PageElements/Banners/bannerSideImage';
+
+function CoCoPage() {
+  return (
+    <DocumentTitle title="Common Components - Digital Government - Province of British Columbia">
+      <div>
+        <p>This is the coco page</p>
+      </div>
+    </DocumentTitle>
+  );
+}
+
+export default CoCoPage;
