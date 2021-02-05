31a51bb86f9f6fbe3cadf963dbdc9448862c9dd3
jsharman <jon.w.sharman@gmail.com>
2021-02-04 20:33:03 -0800

    Fix coco card tests

--- react-frontend/src/components/CoCos/coCoCard.test.js
+++ react-frontend/src/components/CoCos/coCoCard.test.js
@@ -3,29 +3,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-
+import Route from 'react-router-dom';
 import CoCoCard from './coCoCard';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 afterEach(cleanup);
 
 it('Renders without crashing if not props are passed in', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<CoCoCard></CoCoCard>, div);
+  ReactDOM.render(
+    <Router>
+      <CoCoCard></CoCoCard>
+    </Router>,
+    div
+  );
 });
 
 it('Renders all fields when props passed in', () => {
   const { getByTestId } = render(
-    <CoCoCard
-      title="Test Name"
-      description="An awesome description"
-      numberOfUsers="66"
-      onboardingTime="days"
-      supportSchedule="247"
-      cost="Free"
-      status={{ Maintenance: 'ActiveDevelopment', Status: 'On Fire' }}
-      tags={[{ name: 'one' }, { name: 'two' }]}
-      uid="TestUID"
-    />
+    <Router>
+      <CoCoCard
+        title="Test Name"
+        description="An awesome description"
+        numberOfUsers="66"
+        onboardingTime="days"
+        supportSchedule="247"
+        cost="Free"
+        status={{ Maintenance: 'ActiveDevelopment', Status: 'On Fire' }}
+        tags={[{ name: 'one' }, { name: 'two' }]}
+        uid="TestUID"
+      />
+    </Router>
   );
   expect(getByTestId('title')).toHaveTextContent('Test Name');
   expect(getByTestId('description')).toHaveTextContent(
@@ -35,39 +43,43 @@ it('Renders all fields when props passed in', () => {
 
 it('Expect 3 tags displayed when 3 tags passed in', () => {
   const { getAllByTestId } = render(
-    <CoCoCard
-      title="Test Name"
-      description="An awesome description"
-      numberOfUsers="66"
-      onboardingTime="days"
-      supportSchedule="247"
-      cost="Free"
-      status={{ Maintenance: 'ActiveDevelopment', Status: 'On Fire' }}
-      tags={[{ name: 'one' }, { name: 'two' }, { name: 'three' }]}
-      uid="TestUID"
-    />
+    <Router>
+      <CoCoCard
+        title="Test Name"
+        description="An awesome description"
+        numberOfUsers="66"
+        onboardingTime="days"
+        supportSchedule="247"
+        cost="Free"
+        status={{ Maintenance: 'ActiveDevelopment', Status: 'On Fire' }}
+        tags={[{ name: 'one' }, { name: 'two' }, { name: 'three' }]}
+        uid="TestUID"
+      />
+    </Router>
   );
   expect(getAllByTestId('tag-badge').length).toEqual(3);
 });
 
 it('Expect only 3 tags displayed when 4 or more tags passed in', () => {
   const { getAllByTestId } = render(
-    <CoCoCard
-      title="Test Name"
-      description="An awesome description"
-      numberOfUsers="66"
-      onboardingTime="days"
-      supportSchedule="247"
-      cost="Free"
-      status={{ Maintenance: 'ActiveDevelopment', Status: 'On Fire' }}
-      tags={[
-        { name: 'one' },
-        { name: 'two' },
-        { name: 'three' },
-        { name: 'four' },
-      ]}
-      uid="TestUID"
-    />
+    <Router>
+      <CoCoCard
+        title="Test Name"
+        description="An awesome description"
+        numberOfUsers="66"
+        onboardingTime="days"
+        supportSchedule="247"
+        cost="Free"
+        status={{ Maintenance: 'ActiveDevelopment', Status: 'On Fire' }}
+        tags={[
+          { name: 'one' },
+          { name: 'two' },
+          { name: 'three' },
+          { name: 'four' },
+        ]}
+        uid="TestUID"
+      />
+    </Router>
   );
   expect(getAllByTestId('tag-badge').length).toEqual(3);
 });
