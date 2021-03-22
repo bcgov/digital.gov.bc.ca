97bcaca823e1721b2fa39d17335064a434c31ade
jsharman <jon.w.sharman@gmail.com>
2021-03-22 14:56:56 -0700

    Fixed the standards links and converted them to guid based links

---
+++ react-frontend/src/constants/urls.js
@@ -0,0 +1,18 @@
+export const standardsUrls = {
+  AccessiblityandInclusionToolkit:
+    'https://www2.gov.bc.ca/gov/content?id=5D5C4759BC7E494AB9E8EDB7AD3D3D78',
+  MinistryIMITPoliciesAndStandards:
+    'https://www2.gov.bc.ca/gov/content?id=2AC548A48CD7445A9359AF7BB204B6F6',
+  WebStandards:
+    'https://www2.gov.bc.ca/gov/content?id=1AA28225F41347FE9F8D0B4F1733CEFE',
+  PrivacyManagementAccountabilityPolicy:
+    'https://www2.gov.bc.ca/gov/content?id=C962EFF559134B69A8A9ABB3C121D6C7',
+  PrivacyandPersonalInformation:
+    'https://www2.gov.bc.ca/gov/content?id=54A04E35FA854B298DECF17ABC94E943',
+  PrivacyImpactAssessments:
+    'https://www2.gov.bc.ca/gov/content?id=CFA561FF833D42B68FDD9A818ECAFFBE',
+  InformationSecurityPolicyandGuidelines:
+    'https://www2.gov.bc.ca/gov/content?id=BB7D7F3938634BF2973BDE1A899FB755',
+  SecurityThreatandRiskAssessments:
+    'https://www2.gov.bc.ca/gov/content?id=7175C19B66564EA3A343AB8B668BEFC2',
+};
