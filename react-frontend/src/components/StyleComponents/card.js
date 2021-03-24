ae6d9cdefe5ba68d06bd991d6dfcc6d42ce37cff
jsharman <jon.w.sharman@gmail.com>
2021-03-24 09:45:13 -0700

    Revert "Added mark styling"
    This reverts commit b65ee6c50f670b287c71149c97e07ff393f38904.

--- react-frontend/src/components/StyleComponents/card.js
+++ react-frontend/src/components/StyleComponents/card.js
@@ -130,7 +130,7 @@ export const CommunityCardDescription = styled.p.attrs({
   border-radius: 0 0 ${cardBorderRadius} ${cardBorderRadius};
   height: 200px;
   margin-top: auto;
-  margin-bottom: 0;
+  margin-bottom: -2px;
   overflow: hidden;
   padding: 24px;
   text-align: left;
@@ -154,18 +154,14 @@ export const CommunityCardStyled = styled(Link).attrs({
   className: 'cardRound',
 })`
   background: #003366;
-  border-radius: ${cardBorderRadius} ${cardBorderRadius} 30px 30px;
+  border-radius: ${cardBorderRadius};
   height: 95%;
   margin-bottom: 20px;
   width: 100%;
   display: flex;
   flex-direction: column;
   :hover {
-    border: 5px solid #fdb917;
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-    position: relative;
-    right: -7px;
-    top: -7px;
+    box-shadow: 5px 7px grey;
   }
 `;
 
