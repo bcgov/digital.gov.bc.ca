7bdfca59f5082049c99282643af36dff8c2ede27
jsharman <jon.w.sharman@gmail.com>
2021-05-19 16:47:09 -0700

    The app now accepts 3 and less who is using it components on a coco

--- react-frontend/src/components/CoCos/CoCoPageComponents/analytics.js
+++ react-frontend/src/components/CoCos/CoCoPageComponents/analytics.js
@@ -9,38 +9,43 @@ import { Heading, SubHeading } from '../../StyleComponents/headings';
 function Analytics({ coCoName, numberOfUsers, creationDate, whoIsUsingThis }) {
   let ministries = whoIsUsingThis?.map((who) => who?.ministry?.MinistryAcronym);
   let summaries = whoIsUsingThis?.map((who) => who?.Summary);
-
-  if (summaries) {
-    return (
-      <div style={{ marginTop: '60px' }}>
-        {ministries?.length > 0 && (
-          <>
-            <Row>
-              <Col xs={12}>
-                <Heading>Who else is using this?</Heading>
-              </Col>
-            </Row>
-            <Row>
-              <MinistryUse acronym={ministries[0]} summary={summaries[0]} />
-              <MinistryUse acronym={ministries[1]} summary={summaries[1]} />
-              <MinistryUse acronym={ministries[2]} summary={summaries[2]} />
-            </Row>
-          </>
-        )}
-        <Row>
-          <Col xs={6}>
-            <SubHeading>Running Since</SubHeading>
-            <p data-testid="date">{dateFormat(creationDate, 'mmmm yyyy')}</p>
-          </Col>
-          <Col xs={6} style={{ textAlign: 'center' }}>
-            <SubHeading>{numberOfUsers}</SubHeading>
-            <p>teams using {coCoName}</p>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
-  return <div />;
+  console.log(whoIsUsingThis);
+  return (
+    <div style={{ marginTop: '60px' }}>
+      {whoIsUsingThis?.length > 0 && (
+        <>
+          <Row>
+            <Col xs={12}>
+              <Heading>Who else is using this?</Heading>
+            </Col>
+          </Row>
+          <Row>
+            {/* ministries.map() */}
+            {whoIsUsingThis.map((who, i) => (
+              <MinistryUse
+                acronym={who?.ministry?.MinistryAcronym}
+                summary={who?.Summary}
+                key={i}
+              />
+            ))}
+            {/* <MinistryUse acronym={ministries[0]} summary={summaries[0]} />
+             <MinistryUse acronym={ministries[1]} summary={summaries[1]} />
+             <MinistryUse acronym={ministries[2]} summary={summaries[2]} /> */}
+          </Row>
+        </>
+      )}
+      <Row>
+        <Col xs={6}>
+          <SubHeading>Running Since</SubHeading>
+          <p data-testid="date">{dateFormat(creationDate, 'mmmm yyyy')}</p>
+        </Col>
+        <Col xs={6} style={{ textAlign: 'center' }}>
+          <SubHeading>{numberOfUsers}</SubHeading>
+          <p>teams using {coCoName}</p>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default Analytics;
