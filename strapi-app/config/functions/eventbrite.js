953aad55e2285ad66a6d5ef5c968f77c0d858545
jsharman <jon.w.sharman@gmail.com>
2021-06-08 08:11:23 -0700

    remove console log

--- strapi-app/config/functions/eventbrite.js
+++ strapi-app/config/functions/eventbrite.js
@@ -1,8 +1,6 @@
 const axios = require('axios');
 
 module.exports = async () => {
-  console.log("The token is:")
-  console.log(process.env.EVENTBRITE_BEARER_TOKEN)
   const { data } = await axios.get(
     'https://www.eventbriteapi.com/v3/organizations/228490647317/events/',
     {
