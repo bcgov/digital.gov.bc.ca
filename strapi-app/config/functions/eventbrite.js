005b9b833c399b1fb88a961ec05c7da980546d10
jsharman <jon.w.sharman@gmail.com>
2021-06-15 16:24:35 -0700

    Create update delete of events now working

--- strapi-app/config/functions/eventbrite.js
+++ strapi-app/config/functions/eventbrite.js
@@ -9,12 +9,11 @@ const FORMAT_ID_FOR_COURSE = 9;
 //Handle the multiple pages if needed (MAKE SURE IT DOESN'T BREAK OTHER STUFF) DONE MAKE IT ONLY LIVE EVENTS ONCE EVERTHING IS WORKING
 //build out content type:  cover image url, title, description, link to EB
 
-// This populates a list of live eventbrite events 
+// Populate a list of live eventbrite events 
 async function getEvents() {
   let events = [];
   let hasMoreItems = false;
   let continuationToken = null
-
   do {
     // let baseURL = 'https://www.eventbriteapi.com/v3/organizations/228490647317/events'
     let baseURL = 'https://www.eventbriteapi.com/v3/organizations/228490647317/events?status=live';
@@ -31,16 +30,12 @@ async function getEvents() {
 
     hasMoreItems = data.pagination.has_more_items;
     continuationToken = data.pagination.continuation;
-    console.log(`The continuation token is ${continuationToken}`)
-    console.log(data.pagination)
     events.push(...data.events);
 
   } while (hasMoreItems)
 
   return events;
 }
-
-
 
 module.exports = async () => {
   //Get a list of all entries in strapi
@@ -50,22 +45,44 @@ module.exports = async () => {
 
   //Get the current live events from 
   const events = await getEvents();
+  const listEventIDs = events.map(event => event.id);
 
+  // If the event is not in the live event list delete it
+  listSavedEvents.forEach(savedEventID => {
+    if (!listEventIDs.includes(savedEventID)) {
+      strapi.query('eventbrite-event').delete({ EventID: savedEventID })
+      console.log(`deleted event ${savedEventID}`)
+    }
+  })
 
-  console.log(`There are ${events.length} events returned`)
+  // console.log(`There are ${events.length} events returned`)
 
   //TODO
   //If an id is not found when the pull happens delete it from strapi
   events.forEach(event => {
-    // If an event is part of a 
+    //A formatid of 9 is for a course, null or 10 is not
+    const isCourse = (event.format_id == FORMAT_ID_FOR_COURSE)
 
-
-    if (!listSavedEvents.includes(event.id)) {
-      console.log(`The event ID is ${event.id}`)
-
-      //A formatid of 9 is for a course, null or 10 is not
-      const isCourse = (event.format_id == FORMAT_ID_FOR_COURSE)
-
+    if (listSavedEvents.includes(event.id)) {
+      // If an event is already stored, update it if necessary
+      //TODO:check if any event field has actually changed.      
+      strapi
+        .query('eventbrite-event')
+        .update(
+          { EventID: event.id },
+          {
+            Name: event.name.text,
+            Description: event.description.text,
+            Url: event.url,
+            ImageUrl: event.resource_uri,
+            IsCourse: isCourse,
+            IsSeries: event.is_series,
+            StartTime: event.start.utc,
+            SeriesUID: event.series_id
+          });
+      console.log(`updated event ${event.id}`)
+    } else {
+      //If the event is not stored create a new one.
       strapi
         .query('eventbrite-event')
         .create({
@@ -79,6 +96,7 @@ module.exports = async () => {
           StartTime: event.start.utc,
           SeriesUID: event.series_id
         });
+      console.log(`created event ${event.id}`)
     }
 
   });
