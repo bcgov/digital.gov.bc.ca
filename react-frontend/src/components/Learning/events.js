b79bfe9e4852438d2053c6159ebe9864f79d4919
jsharman <jon.w.sharman@gmail.com>
2021-06-23 09:27:19 -0700

    Configure the strapi collection type labels implement the event query

--- react-frontend/src/components/Learning/events.js
+++ react-frontend/src/components/Learning/events.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import { Col, Row } from 'react-flexbox-grid';
+import Query from '../Query';
+import EVENTS_QUERY from '../../queries/eventbrite/events';
 
 import { ContentBlockContainer } from '../StyleComponents/pageContent';
 import { Heading } from '../StyleComponents/headings';
+import { StyledP, HrefLinkStandalone } from '../StyleComponents/htmlTags';
 
 function Events() {
   return (
-    <ContentBlockContainer id="courses">
+    <ContentBlockContainer id="standards">
       <Row>
         <Col sm={12}>
-          <Heading>Events</Heading>
+          <Heading>Courses</Heading>
         </Col>
       </Row>
+      <Query query={EVENTS_QUERY}>
+        {({ data: { eventbriteEvents } }) => {
+          console.log(eventbriteEvents);
+          return (
+            <Row style={{ marginBottom: '4px' }}>
+              {/* {eventbriteEvents.map((event, i) => {
+                console.log(event)
+                return (
+                  // <Col sm={12} md={6} key={coCo.uid}>
+                  //   <CoCoCard
+                  //     title={coCo.Name}
+                  //     description={coCo.ShortDescription}
+                  //     nameAndMinistry={coCo.TeamNameAndMinistry}
+                  //     numberOfUsers={coCo.NumberOfUsers}
+                  //     onboardingTime={coCo.OnboardingTime}
+                  //     supportSchedule={coCo.SupportSchedule}
+                  //     cost={coCo.CostStructure.Cost}
+                  //     status={coCo.ProjectStatus}
+                  //     tags={coCo.Tags}
+                  //     uid={coCo.uid}
+                  //   />
+                  // </Col>
+                );
+              })} */}
+            </Row>
+          );
+        }}
+      </Query>
     </ContentBlockContainer>
   );
 }
