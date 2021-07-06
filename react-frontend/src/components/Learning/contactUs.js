94af03e934253e5c3fcea2db8b1185c35b8ab41c
jsharman <jon.w.sharman@gmail.com>
2021-07-06 11:41:00 -0700

    added bottom margin to learning admin picture

--- react-frontend/src/components/Learning/contactUs.js
+++ react-frontend/src/components/Learning/contactUs.js
@@ -41,6 +41,7 @@ function ContactUs() {
                           width: '100%',
                           borderRadius: '18px',
                           maxWidth: '300px',
+                          marginBottom: '16px',
                         }}
                       />
                       <p style={{ marginBottom: '0', maxWidth: '300px' }}>
