535e4630430bb5f29afc966569637790f38a00ab
Mark Wilson <Mark.Wilson@gov.bc.ca>
2021-08-25 13:55:53 -0700

    Update footer links

--- react-frontend/src/components/Nav/footer.js
+++ react-frontend/src/components/Nav/footer.js
@@ -17,45 +17,16 @@ const Footer = () => {
             <FooterLinkInternal to="/">Home</FooterLinkInternal>
           </FooterListItem>
           <FooterListItem>
-            <FooterLink
-              href={footerLinks.disclaimer}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Disclaimer
-            </FooterLink>
+            <FooterLinkInternal to="/disclaimer">Disclaimer</FooterLinkInternal>
           </FooterListItem>
           <FooterListItem>
-            <FooterLink
-              href={footerLinks.privacy}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Privacy
-            </FooterLink>
+            <FooterLinkInternal to="/privacy">Privacy</FooterLinkInternal>
           </FooterListItem>
           <FooterListItem>
-            <FooterLink
-              href={footerLinks.accessibility}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Accessibility
-            </FooterLink>
+            <FooterLinkInternal to="/copyright">Copyright</FooterLinkInternal>
           </FooterListItem>
           <FooterListItem>
-            <FooterLink
-              href={footerLinks.copyright}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Copyright
-            </FooterLink>
-          </FooterListItem>
-          <FooterListItem>
-            <FooterLink href="mailto:digital.government@gov.bc.ca">
-              Contact Us
-            </FooterLink>
+            <FooterLinkInternal to="/about">About</FooterLinkInternal>
           </FooterListItem>
         </FooterUL>
       </FooterContainer>
