081dcd9924d98dc3b0e8802b64ed52a788766059
jsharman <jon.w.sharman@gmail.com>
2021-08-31 10:27:40 -0700

    WIP

---
+++ react-frontend/url-checker/url.test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+import {
+  videoLinks,
+  standardsUrls,
+  resourceLinks,
+  podcastUrls,
+  hostingOptionUrls,
+  footerLinks,
+  digitalCardLinks,
+  digitalPrincipleUrls,
+  developerLinks,
+  designersUrls,
+  definingLinks,
+  consideringLinks,
+  collabResources,
+  collabToolUrls,
+  assemblingLinks
+} from '../src/constants/urls'
+
+const linkCheck = require('link-check');
+
+
+
+it('Checks a link', () => {
+
+  linkCheck(assemblingLinks.exchangeLab, function (err, result) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(`${result.link} is ${result.status}`);
+  });
+
+});
