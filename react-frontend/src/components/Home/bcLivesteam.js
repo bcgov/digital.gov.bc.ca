820a3ff99469e30490b52af7bbc99faabbad11c0
jsharman <jon.w.sharman@gmail.com>
2021-09-21 16:11:30 -0700

    added conditional rendering

--- react-frontend/src/components/Home/bcLivesteam.js
+++ react-frontend/src/components/Home/bcLivesteam.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import InfoCard from './infocard';
+import dateFormat from 'dateformat';
 import { Col, Row } from 'react-flexbox-grid';
 import { PageContainer } from '../StyleComponents/pageContent';
 import { Heading } from '../StyleComponents/headings';
@@ -15,54 +15,73 @@ const BCLivestream = () => {
     'strapiMediaUrl'
   ];
   return (
-    <div style={{ backgroundColor: '#fff' }}>
-      <PageContainer style={{ backgroundColor: '#fff', paddingTop: '40px' }}>
-        <Query query={LIVESTREAM_QUERY}>
-          {({ data: { digitalLivestreamContact, digitalBcLivestreams } }) => {
-            console.log(digitalLivestreamContact);
-            console.log(digitalBcLivestreams);
-            const imageSource =
-              strapiMediaUrl + digitalBcLivestreams[0]?.EventImage?.url;
-
-            return (
-              <Row between="xs">
-                <Col sm={12} md={3}>
-                  <Heading>#DigitalBC Livestream</Heading>
-                  <p>
-                    Watch live broadcasts of monthly updates, webinars, sprint
-                    reviews, and more!
-                  </p>
-                  <LinkExternalButton
-                    href={digitalLivestreamContact?.SubscriptionURL}
-                    style={{ maxWidth: '160px' }}
-                  >
-                    Subscribe
-                  </LinkExternalButton>
-                </Col>
-                <Col sm={12} md={5}>
-                  <img src={imageSource} style={{ width: '100%' }} />
-                </Col>
-                <Col sm={12} md={4} style={{ textAlign: 'center' }}>
-                  <p>
-                    <b>Next broadcast</b>
-                  </p>
-                  <p>{digitalBcLivestreams[0]?.BroadcastTime}</p>
-                  <p style={{ textAlign: 'left' }}>
-                    {digitalBcLivestreams[0]?.Description}
-                  </p>
-                  <p>Watch past broadcasts</p>
-                  <img
-                    src={youTubeLogo}
-                    style={{ height: '20px', width: '80px' }}
-                  />
-                  {/* add the youtube icon */}
-                </Col>
-              </Row>
-            );
-          }}
-        </Query>
-      </PageContainer>
-    </div>
+    <Query query={LIVESTREAM_QUERY}>
+      {({ data: { digitalLivestreamContact, digitalBcLivestreams } }) => {
+        const imageSource =
+          strapiMediaUrl + digitalBcLivestreams[0]?.EventImage?.url;
+        // if no next event is provided in strapi, this section will not render
+        const now = new Date();
+        const nextEventTime = new Date(digitalBcLivestreams[0]?.BroadcastTime);
+        if (nextEventTime && nextEventTime > now) {
+          return (
+            <div style={{ backgroundColor: '#fff' }}>
+              <PageContainer
+                style={{ backgroundColor: '#fff', paddingTop: '40px' }}
+              >
+                <Row between="xs">
+                  <Col sm={12} md={3}>
+                    <Heading>#DigitalBC Livestream</Heading>
+                    <p>
+                      Watch live broadcasts of monthly updates, webinars, sprint
+                      reviews, and more!
+                    </p>
+                    {/* Only render if the link is provided */}
+                    {digitalLivestreamContact?.SubscriptionURL && (
+                      <LinkExternalButton
+                        href={digitalLivestreamContact?.SubscriptionURL}
+                        style={{ maxWidth: '160px' }}
+                      >
+                        Subscribe
+                      </LinkExternalButton>
+                    )}
+                  </Col>
+                  <Col sm={12} md={5}>
+                    <img src={imageSource} style={{ width: '100%' }} />
+                  </Col>
+                  <Col sm={12} md={4} style={{ textAlign: 'center' }}>
+                    <p>
+                      <b>Next broadcast</b>
+                    </p>
+                    <p>
+                      {dateFormat(
+                        digitalBcLivestreams[0]?.BroadcastTime,
+                        'ddd, mmm d - hh:MM TT'
+                      ) + ' PST'}
+                    </p>
+                    <p style={{ textAlign: 'left' }}>
+                      {digitalBcLivestreams[0]?.Description}
+                    </p>
+                    {/* Only render if link provided */}
+                    {digitalLivestreamContact?.PastEpisodeURL && (
+                      <div>
+                        <p>Watch past broadcasts</p>
+                        <a href={digitalLivestreamContact?.PastEpisodeURL}>
+                          <img
+                            src={youTubeLogo}
+                            style={{ height: '20px', width: '80px' }}
+                          />
+                        </a>
+                      </div>
+                    )}
+                  </Col>
+                </Row>
+              </PageContainer>
+            </div>
+          );
+        }
+        return <div />;
+      }}
+    </Query>
   );
 };
 
