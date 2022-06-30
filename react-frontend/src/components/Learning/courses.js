bc34ce3b54477c5b4e560c0c0870861106fe937a
kmandryk <kaeganmandryk@gmail.com>
2022-06-30 15:57:03 -0700

    Update courses.js

--- react-frontend/src/components/Learning/courses.js
+++ react-frontend/src/components/Learning/courses.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Col, Row } from 'react-flexbox-grid';
 
 import { ContentBlockContainer } from '../StyleComponents/pageContent';
-import { Heading } from '../StyleComponents/headings';
+import { Heading, SubHeading } from '../StyleComponents/headings';
+import { StyledP, HrefLink } from '../StyleComponents/htmlTags';
 import EventCards from './eventCards';
 
 // The courses are pulled from eventbrite and the content populates cards
@@ -12,6 +13,15 @@ function Courses() {
       <Row>
         <Col sm={12}>
           <Heading>Courses</Heading>
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={12}>
+          <SubHeading>Digital Academy LearningHub</SubHeading>
+          <SubTitle>Visit the <HrefLink>Digital Academy LearningHub</HrefLink> page to sign-up for scheduled:</SubTitle>
+          <StyledP><li>internal courses related to Agile, OpenShift, Scrum Master, and Scrum Product Owner</li>
+            <li>external partner learning opportunities on a variety of topics</li></StyledP>
+          <SubTitle>For more information contact <HrefLink href="mailto:DigitalAcademy@gov.bc.ca">DigitalAcademy@gov.bc.ca</HrefLink></SubTitle>
         </Col>
       </Row>
       <EventCards isCourse={true} />
