89de276b41e825f49eeaff5ab213080538418d49
kmandryk <kaeganmandryk@gmail.com>
2022-07-05 09:38:08 -0700

    Update learning.js
    add digital academy content to learning page

--- react-frontend/src/components/Learning/learning.js
+++ react-frontend/src/components/Learning/learning.js
@@ -4,9 +4,13 @@ import ContactUs from './contactUs';
 import Courses from './courses';
 import Events from './events';
 import Podcast from './podcast';
-
+import { SubHeading } from '../StyleComponents/headings';
+import { StyledP, HrefLink } from '../StyleComponents/htmlTags';
 import DocumentTitle from 'react-document-title';
 import BannerSideImage from '../PageElements/Banners/bannerSideImage';
+import { Col, Row } from 'react-flexbox-grid';
+
+import { ContentBlockContainer } from '../StyleComponents/pageContent';
 
 const learningImage =
   require('../../images/pngIllustrations/learningWhite.png').default;
@@ -22,6 +26,21 @@ function Learning() {
           }
           image={learningImage}
         />
+        <ContentBlockContainer id="digitalacademy">
+          <Row>
+            <Col sm={12}>
+              <SubHeading>Digital Academy LearningHub</SubHeading>
+              Visit the <HrefLink>Digital Academy LearningHub</HrefLink> page to sign-up for scheduled:
+              <StyledP>
+                <ul>
+                  <li>internal courses related to Agile, OpenShift, Scrum Master, and Scrum Product Owner</li>
+                  <li>external partner learning opportunities on a variety of topics</li>
+                </ul>
+              </StyledP>
+              For more information contact <HrefLink href="mailto:DigitalAcademy@gov.bc.ca">DigitalAcademy@gov.bc.ca</HrefLink>
+            </Col>
+          </Row>
+        </ContentBlockContainer>
         <Courses />
         <Events />
         <ContactUs />
