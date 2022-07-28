9e30a52db9ddf53c2cc079ad01e382cfff45d664
Struk <Alex.Struk@gov.bc.ca>
2022-07-28 16:47:56 -0700

    initial commit
    Cucumber integration using cypress-cucumber-preprocessor

---
+++ react-frontend/cypress/integration/test_sample_cuc/test_sample_cuc.js
@@ -0,0 +1,7 @@
+import { When, Then } from 'cypress-cucumber-preprocessor/steps'
+When('I visit home screen', () => {
+    cy.visit('/');
+})
+Then('the title should say "Digital Government"', () => {
+    cy.get('.navTitle').should('have.text',' Digital Government');
+})
