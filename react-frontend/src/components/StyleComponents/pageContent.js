80879bdccd7d350aa8abe1acd4ef27157f33e5dc
Mandryk <Kaegan.Mandryk@gov.bc.ca>
2022-10-14 17:03:38 -0700

    re-open PR for prod deployment

--- react-frontend/src/components/StyleComponents/pageContent.js
+++ react-frontend/src/components/StyleComponents/pageContent.js
@@ -101,7 +101,7 @@ export const NavBarContainer = styled(HorizontalContainer).attrs({
   className: 'navBar',
 })`
   z-index: 0;
-  max-width: 1200px;
+  max-width: 1200px; 
 `;
 
 export const RouteBody = styled.div.attrs({
